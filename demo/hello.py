4b2516a9db25fb9ab6f96586eee9541ebc28f729
srikanthpragada <srikanthpragada@gmail.com>
2022-11-04 16:55:42 +0530

    03-NOV

---
+++ demo/hello.py
@@ -0,0 +1,3 @@
+# Say hello
+name = input("Enter name :")
+print("Hello", name)
---
+++ demo/net_price.py
@@ -0,0 +1,6 @@
+# Take price and display net price after a discount of 10%
+
+price = int(input("Enter price :"))
+discount = price * 0.10
+net_price = price - discount
+print('Net Price :', net_price)
