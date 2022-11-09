75f8ae856fb7a10bdf29437899acaaa2b79b0803
srikanthpragada <srikanthpragada@gmail.com>
2022-11-09 19:57:24 +0530

    09-NOV

---
+++ demo/assignments/word_per_line.py
@@ -0,0 +1,7 @@
+st = "This is to test"
+
+for c in st:
+    if c.isspace():
+        print()
+    else:
+        print(c, end='')
---
+++ demo/ds/list_comp.py
@@ -0,0 +1,11 @@
+from setuptools._distutils.util import split_quoted
+
+squares = []
+for n in range(1, 11):
+    squares.append(n * n)
+
+print(squares)
+
+# List Comprehension
+squares = [n*n for n in range(1,11) if n % 2 == 0]
+print(squares)
